b1c881aadc50ded6763550257b6137de7856dd0e
MaheshNamal-droid <43595358+MaheshNamal@users.noreply.github.com>
2024-12-21 19:23:03 +0530

    admin panel
    admin panel manage promotion  promotion

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -62,8 +62,11 @@ class PromotionController extends Controller
     // get all promotions
     public function getPromotions()
     {
-        $promotions = promotion::where('status', 1)->paginate(10);
-        return Response::json($promotions);
+        $promotions = promotion::where('status', 1)->paginate(2);
+       // return Response::json($promotions);
+        return Inertia::render('Admininistrator/Promotions', ['posts' => $promotions]);
+        // $promotions = promotion::where('status', 1)->paginate(10);
+        // return Response::json($promotions);
     } 
     
     public function getRandomPromotion(){
