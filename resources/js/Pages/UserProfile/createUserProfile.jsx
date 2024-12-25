52dfa97b9593f23143c6e3b5ddd1bbdf344aef2d
sanduni28 <s22010154@ousl.lk>
2024-12-25 13:40:36 +0530

    Updated user profile form UI

--- resources/js/Pages/UserProfile/createUserProfile.jsx
+++ resources/js/Pages/UserProfile/createUserProfile.jsx
@@ -76,76 +76,76 @@ export default function createUserProfile({ auth }) {
           <div>
             <form onSubmit={handleSubmit} className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md space-y-4">
               <div>
-                <h3 className="block text-gray-700 font-semibold mb-2 text-lg text-center">New User Profile</h3>
+                <h3 className="block text-[#254336] font-semibold mb-2 text-xl text-center">New User Profile</h3>
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2"> First Name:</label>
+                <label className="block text-[#254336] font-semibold mb-2"> First Name:</label>
                 <input
                   type="text"
                   name="first_name"
                   value={formData.first_name}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                  className="w-full px-4 py-2 border border-[#41B06E] rounded-md focus:outline-none focus:border-indigo-500"
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Last Name:</label>
+                <label className="block text-[#254336] font-semibold mb-2">Last Name:</label>
                 <input
                   type="text"
                   name="last_name"
                   value={formData.last_name}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                  className="w-full px-4 py-2 border border-[#41B06E] rounded-md focus:outline-none focus:border-indigo-500"
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Email:</label>
+                <label className="block text-[#254336] font-semibold mb-2">Email:</label>
                 <input
                   type="text"
                   name="email"
                   value={formData.email}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                  className="w-full px-4 py-2 border border-[#41B06E] rounded-md focus:outline-none focus:border-indigo-500"
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Address:</label>
+                <label className="block text-[#254336] font-semibold mb-2">Address:</label>
                 <input
                   type="text"
                   name="address"
                   value={formData.address}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                  className="w-full px-4 py-2 border border-[#41B06E] rounded-md focus:outline-none focus:border-indigo-500"
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Phone Number:</label>
+                <label className="block text-[#254336] font-semibold mb-2">Phone Number:</label>
                 <input
                   type="text"
                   name="contact_phone"
                   value={formData.contact_phone}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                  className="w-full px-4 py-2 border border-[#41B06E] rounded-md focus:outline-none focus:border-indigo-500"
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Social Media:</label>
+                <label className="block text-[#254336] font-semibold mb-2">Social Media:</label>
                 <input
                   type="url"
                   name="social_media"
                   value={formData.social_media}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                  className="w-full px-4 py-2 border border-[#41B06E] rounded-md focus:outline-none focus:border-indigo-500"
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Profile Picture:</label>
+                <label className="block text-[#254336] font-semibold mb-2">Profile Picture:</label>
                 <input
                   type="file"
                   name="profile_picture"
@@ -155,7 +155,7 @@ export default function createUserProfile({ auth }) {
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Resume:</label>
+                <label className="block text-[#254336] font-semibold mb-2">Resume:</label>
                 <input
                   type="file"
                   name="resume"
@@ -165,19 +165,19 @@ export default function createUserProfile({ auth }) {
                 />
               </div>
               <div>
-                <label className="block text-gray-700 font-semibold mb-2">Description:</label>
-                <input
+                <label className="block text-[#254336] font-semibold mb-2">Description:</label>
+                <textarea
                   type="text"
                   name="description"
                   value={formData.description}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                  className="w-full px-4 py-2 border border-[#41B06E] rounded-md focus:outline-none focus:border-indigo-500"
                 />
               </div>
               <button
                 type="submit"
-                className="w-full bg-indigo-600 text-white font-semibold py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                className="w-full bg-[#1A5319] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#254336] focus:outline-none focus:ring-2 focus:ring-indigo-500"
               >
                 Submit
               </button>
